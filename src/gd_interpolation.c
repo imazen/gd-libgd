1c94911f23d9846230370eb245982047e33b93eb
Chris Reuter <chris@blit.ca>
2013-08-30 14:43:36 -0400

    Added another interpolation (GD_BICUBIC_FIXED2) which is just a copy of bicubic fixed.

--- src/gd_interpolation.c
+++ src/gd_interpolation.c
@@ -1622,7 +1622,243 @@ gdImagePtr gdImageScaleBicubicFixed(gdImagePtr src, const unsigned int width, co
 		dst_offset_y++;
 	}
 	return dst;
-}
+}/* gdImageScaleBicubicFixed*/
+
+gdImagePtr gdImageScaleBicubicFixed2(gdImagePtr src, const unsigned int width, const unsigned int height)
+{
+	const long new_width = MAX(1, width);
+	const long new_height = MAX(1, height);
+	const int src_w = gdImageSX(src);
+	const int src_h = gdImageSY(src);
+	const gdFixed f_dx = gd_ftofx((float)src_w / (float)new_width);
+	const gdFixed f_dy = gd_ftofx((float)src_h / (float)new_height);
+	const gdFixed f_1 = gd_itofx(1);
+	const gdFixed f_2 = gd_itofx(2);
+	const gdFixed f_4 = gd_itofx(4);
+	const gdFixed f_6 = gd_itofx(6);
+	const gdFixed f_gamma = gd_ftofx(1.04f);
+	gdImagePtr dst;
+
+	unsigned int dst_offset_x;
+	unsigned int dst_offset_y = 0;
+	long i;
+
+	/* impact perf a bit, but not that much. Implementation for palette
+	   images can be done at a later point.
+	*/
+	if (src->trueColor == 0) {
+		gdImagePaletteToTrueColor(src);
+	}
+
+	dst = gdImageCreateTrueColor(new_width, new_height);
+	if (!dst) {
+		return NULL;
+	}
+
+	dst->saveAlphaFlag = 1;
+
+	for (i=0; i < new_height; i++) {
+		long j;
+		dst_offset_x = 0;
+
+		for (j=0; j < new_width; j++) {
+			const gdFixed f_a = gd_mulfx(gd_itofx(i), f_dy);
+			const gdFixed f_b = gd_mulfx(gd_itofx(j), f_dx);
+			const long m = gd_fxtoi(f_a);
+			const long n = gd_fxtoi(f_b);
+			const gdFixed f_f = f_a - gd_itofx(m);
+			const gdFixed f_g = f_b - gd_itofx(n);
+			unsigned int src_offset_x[16], src_offset_y[16];
+			long k;
+			register gdFixed f_red = 0, f_green = 0, f_blue = 0, f_alpha = 0;
+			unsigned char red, green, blue, alpha = 0;
+			int *dst_row = dst->tpixels[dst_offset_y];
+
+			if ((m < 1) || (n < 1)) {
+				src_offset_x[0] = n;
+				src_offset_y[0] = m;
+			} else {
+				src_offset_x[0] = n - 1;
+				src_offset_y[0] = m;
+			}
+
+			if (m < 1) {
+				src_offset_x[1] = n;
+				src_offset_y[1] = m;
+			} else {
+				src_offset_x[1] = n;
+				src_offset_y[1] = m;
+			}
+
+			if ((m < 1) || (n >= src_w - 1)) {
+				src_offset_x[2] = n;
+				src_offset_y[2] = m;
+			} else {
+				src_offset_x[2] = n + 1;
+				src_offset_y[2] = m;
+			}
+
+			if ((m < 1) || (n >= src_w - 2)) {
+				src_offset_x[3] = n;
+				src_offset_y[3] = m;
+			} else {
+				src_offset_x[3] = n + 1 + 1;
+				src_offset_y[3] = m;
+			}
+
+			if (n < 1) {
+				src_offset_x[4] = n;
+				src_offset_y[4] = m;
+			} else {
+				src_offset_x[4] = n - 1;
+				src_offset_y[4] = m;
+			}
+
+			src_offset_x[5] = n;
+			src_offset_y[5] = m;
+			if (n >= src_w-1) {
+				src_offset_x[6] = n;
+				src_offset_y[6] = m;
+			} else {
+				src_offset_x[6] = n + 1;
+				src_offset_y[6] = m;
+			}
+
+			if (n >= src_w - 2) {
+				src_offset_x[7] = n;
+				src_offset_y[7] = m;
+			} else {
+				src_offset_x[7] = n + 1 + 1;
+				src_offset_y[7] = m;
+			}
+
+			if ((m >= src_h - 1) || (n < 1)) {
+				src_offset_x[8] = n;
+				src_offset_y[8] = m;
+			} else {
+				src_offset_x[8] = n - 1;
+				src_offset_y[8] = m;
+			}
+
+			if (m >= src_h - 1) {
+				src_offset_x[8] = n;
+				src_offset_y[8] = m;
+			} else {
+				src_offset_x[9] = n;
+				src_offset_y[9] = m;
+			}
+
+			if ((m >= src_h-1) || (n >= src_w-1)) {
+				src_offset_x[10] = n;
+				src_offset_y[10] = m;
+			} else {
+				src_offset_x[10] = n + 1;
+				src_offset_y[10] = m;
+			}
+
+			if ((m >= src_h - 1) || (n >= src_w - 2)) {
+				src_offset_x[11] = n;
+				src_offset_y[11] = m;
+			} else {
+				src_offset_x[11] = n + 1 + 1;
+				src_offset_y[11] = m;
+			}
+
+			if ((m >= src_h - 2) || (n < 1)) {
+				src_offset_x[12] = n;
+				src_offset_y[12] = m;
+			} else {
+				src_offset_x[12] = n - 1;
+				src_offset_y[12] = m;
+			}
+
+			if (m >= src_h - 2) {
+				src_offset_x[13] = n;
+				src_offset_y[13] = m;
+			} else {
+				src_offset_x[13] = n;
+				src_offset_y[13] = m;
+			}
+
+			if ((m >= src_h - 2) || (n >= src_w - 1)) {
+				src_offset_x[14] = n;
+				src_offset_y[14] = m;
+			} else {
+				src_offset_x[14] = n + 1;
+				src_offset_y[14] = m;
+			}
+
+			if ((m >= src_h - 2) || (n >= src_w - 2)) {
+				src_offset_x[15] = n;
+				src_offset_y[15] = m;
+			} else {
+				src_offset_x[15] = n  + 1 + 1;
+				src_offset_y[15] = m;
+			}
+
+			for (k = -1; k < 3; k++) {
+				const gdFixed f = gd_itofx(k)-f_f;
+				const gdFixed f_fm1 = f - f_1;
+				const gdFixed f_fp1 = f + f_1;
+				const gdFixed f_fp2 = f + f_2;
+				register gdFixed f_a = 0, f_b = 0, f_d = 0, f_c = 0;
+				register gdFixed f_RY;
+				int l;
+
+				if (f_fp2 > 0) f_a = gd_mulfx(f_fp2, gd_mulfx(f_fp2,f_fp2));
+				if (f_fp1 > 0) f_b = gd_mulfx(f_fp1, gd_mulfx(f_fp1,f_fp1));
+				if (f > 0)     f_c = gd_mulfx(f, gd_mulfx(f,f));
+				if (f_fm1 > 0) f_d = gd_mulfx(f_fm1, gd_mulfx(f_fm1,f_fm1));
+
+				f_RY = gd_divfx((f_a - gd_mulfx(f_4,f_b) + gd_mulfx(f_6,f_c) - gd_mulfx(f_4,f_d)),f_6);
+
+				for (l = -1; l < 3; l++) {
+					const gdFixed f = gd_itofx(l) - f_g;
+					const gdFixed f_fm1 = f - f_1;
+					const gdFixed f_fp1 = f + f_1;
+					const gdFixed f_fp2 = f + f_2;
+					register gdFixed f_a = 0, f_b = 0, f_c = 0, f_d = 0;
+					register gdFixed f_RX, f_R, f_rs, f_gs, f_bs, f_ba;
+					register int c;
+					const int _k = ((k+1)*4) + (l+1);
+
+					if (f_fp2 > 0) f_a = gd_mulfx(f_fp2,gd_mulfx(f_fp2,f_fp2));
+
+					if (f_fp1 > 0) f_b = gd_mulfx(f_fp1,gd_mulfx(f_fp1,f_fp1));
+
+					if (f > 0) f_c = gd_mulfx(f,gd_mulfx(f,f));
+
+					if (f_fm1 > 0) f_d = gd_mulfx(f_fm1,gd_mulfx(f_fm1,f_fm1));
+
+					f_RX = gd_divfx((f_a-gd_mulfx(f_4,f_b)+gd_mulfx(f_6,f_c)-gd_mulfx(f_4,f_d)),f_6);
+					f_R = gd_mulfx(f_RY,f_RX);
+
+					c = src->tpixels[*(src_offset_y + _k)][*(src_offset_x + _k)];
+					f_rs = gd_itofx(gdTrueColorGetRed(c));
+					f_gs = gd_itofx(gdTrueColorGetGreen(c));
+					f_bs = gd_itofx(gdTrueColorGetBlue(c));
+					f_ba = gd_itofx(gdTrueColorGetAlpha(c));
+
+					f_red += gd_mulfx(f_rs,f_R);
+					f_green += gd_mulfx(f_gs,f_R);
+					f_blue += gd_mulfx(f_bs,f_R);
+					f_alpha += gd_mulfx(f_ba,f_R);
+				}
+			}
+
+			red    = (unsigned char) CLAMP(gd_fxtoi(gd_mulfx(f_red,   f_gamma)),  0, 255);
+			green  = (unsigned char) CLAMP(gd_fxtoi(gd_mulfx(f_green, f_gamma)),  0, 255);
+			blue   = (unsigned char) CLAMP(gd_fxtoi(gd_mulfx(f_blue,  f_gamma)),  0, 255);
+			alpha  = (unsigned char) CLAMP(gd_fxtoi(gd_mulfx(f_alpha,  f_gamma)), 0, 127);
+
+			*(dst_row + dst_offset_x) = gdTrueColorAlpha(red, green, blue, alpha);
+
+			dst_offset_x++;
+		}
+		dst_offset_y++;
+	}
+	return dst;
+}/* gdImageScaleBicubicFixed2*/
 
 BGD_DECLARE(gdImagePtr) gdImageScale(const gdImagePtr src, const unsigned int new_width, const unsigned int new_height)
 {
@@ -1639,11 +1875,16 @@ BGD_DECLARE(gdImagePtr) gdImageScale(const gdImagePtr src, const unsigned int ne
 			break;
 
 		case GD_BILINEAR_FIXED:
+            printf("bilinear fixed!\n");
 			im_scaled = gdImageScaleBilinear(src, new_width, new_height);
 			break;
 
 		case GD_BICUBIC_FIXED:
 			im_scaled = gdImageScaleBicubicFixed(src, new_width, new_height);
+			break;
+
+		case GD_BICUBIC_FIXED2:
+			im_scaled = gdImageScaleBicubicFixed2(src, new_width, new_height);
 			break;
 
 		/* generic */
@@ -2518,6 +2759,7 @@ BGD_DECLARE(int) gdImageSetInterpolationMethod(gdImagePtr im, gdInterpolationMet
 		case GD_BICUBIC_FIXED:
 		case GD_NEAREST_NEIGHBOUR:
 		case GD_WEIGHTED4:
+		case GD_BICUBIC_FIXED2:
 			im->interpolation = NULL;
 			break;
 
