f4c14d6e0e7af5ce1fd6bb6f990dbc5cc0e04c14
Chris Reuter <chris@blit.ca>
2013-10-03 23:30:59 -0400

    Fixed trivial scale.
    When doing a resize and the source and destination images are the same size
    in one dimension, the code used to just do a memcpy (incorrectly).  Previous
    changes removed this and asserted against the case.  This change handles the
    case more intelligently and minimizes the number of copies that are made
    when this happens.

--- src/gd_interpolation.c
+++ src/gd_interpolation.c
@@ -1062,25 +1062,42 @@ gdImageScaleTwoPass(const gdImagePtr src, const unsigned int new_width,
 {
     const unsigned int src_width = src->sx;
     const unsigned int src_height = src->sy;
-	gdImagePtr tmp_im;
-	gdImagePtr dst;
+	gdImagePtr tmp_im = NULL;;
+	gdImagePtr dst = NULL;
 
-	tmp_im = gdImageCreateTrueColor(new_width, src_height);
-	if (tmp_im == NULL) {
-		return NULL;
-	}
-	gdImageSetInterpolationMethod(tmp_im, src->interpolation_id);
+    /* First, handle the trivial case. */
+    if (src_width == new_width && src_height == new_height) {
+        return gdImageClone(src);
+    }/* if */
 
-	_gdScalePass(src, src_width, tmp_im, new_width, src_height, HORIZONTAL);
+    /* Scale horizontally unless sizes are the same. */
+    if (src_width == new_width) {
+        tmp_im = src;
+    } else {
+        tmp_im = gdImageCreateTrueColor(new_width, src_height);
+        if (tmp_im == NULL) {
+            return NULL;
+        }
+        gdImageSetInterpolationMethod(tmp_im, src->interpolation_id);
 
+        _gdScalePass(src, src_width, tmp_im, new_width, src_height, HORIZONTAL);
+    }/* if .. else*/
+
+    /* If vertical sizes match, we're done. */
+    if (src_height == new_height) {
+        assert(tmp_im != src);
+        return tmp_im;
+    }/* if */
+
+    /* Otherwise, we need to scale vertically. */
 	dst = gdImageCreateTrueColor(new_width, new_height);
-	if (dst == NULL) {
-		gdFree(tmp_im);
-		return NULL;
-	}
+	if (dst != NULL) {
+        _gdScalePass(tmp_im, src_height, dst, new_height, new_width, VERTICAL);
+    }/* if */
 
-	_gdScalePass(tmp_im, src_height, dst, new_height, new_width, VERTICAL);
-	gdFree(tmp_im);
+    if (src != tmp_im) {
+        gdFree(tmp_im);
+    }/* if */
 
 	return dst;
 }
